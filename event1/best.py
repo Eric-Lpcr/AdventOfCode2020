2a4511edc90a944bf8627170c668d53e609fc908
Eric-Lpcr <lepicier.eric@gmail.com>
2020-12-02 09:00:20 +0100

    Best solutions someone else found

--- event1/best.py
+++ event1/best.py
@@ -1,5 +1,5 @@
 # https://github.com/mdumke/aoc2020/blob/main/day01/main.py
-
+import math
 from itertools import combinations
 
 
@@ -8,3 +8,12 @@ def sum_to(target: int, n: int, values: [int]) -> [int]:
     for group in combinations(values, n):
         if sum(group) == target:
             return group
+
+
+print('part 1:', math.prod(sum_to(2020, 2, values)))
+
+
+# unknown source
+
+def solution(target, n, data):
+    return next(math.prod(c) for c in combinations(data, n) if sum(c) == target)
