da58b590e15059bcf069d704a01e97d53d19177a
Eric-Lpcr <lepicier.eric@gmail.com>
2020-12-02 09:30:04 +0100

    Remove unused import

--- event2/event2.py
+++ event2/event2.py
@@ -1,6 +1,3 @@
-import time
-
-
 def password_is_valid_rule1(line):
     minimum, maximum, letter, password = line.replace('-', ' ').replace(':', ' ').split()
     return  int(minimum) <= password.count(letter) <= int(maximum)
