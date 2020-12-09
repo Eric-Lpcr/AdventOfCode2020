f3faccab4b191c1fca93cca5df0ba1f4cf8109ab
Eric-Lpcr <lepicier.eric@gmail.com>
2020-12-09 13:16:14 +0100

    Minor correction

--- day9/day9.py
+++ day9/day9.py
@@ -10,7 +10,7 @@ def find_sum_sequence(number, numbers):
     """Returns a sequence of at least two numbers summing up to number"""
     found_seq = None
     i = 0
-    while i < len(numbers) and not found_seq:
+    while i < len(numbers)-1 and not found_seq:
         sum_up = list(accumulate(numbers[i:]))
         if number in sum_up[1:]:  # Consider sum of at least *two* numbers
             index = sum_up.index(number)  # index+1 is the number of accumulated values in sum_up
@@ -22,10 +22,9 @@ def find_sum_sequence(number, numbers):
 def main():
     with open('input.txt') as f:
         port_output = (int(line) for line in f.readlines())
-
     preamble_length = 25
-    stack = list(islice(port_output, preamble_length + 1))  # stack contains preamble and next number to be tested
 
+    stack = list(islice(port_output, preamble_length + 1))  # stack contains preamble and next number to be tested
     while is_a_sum_of_two(stack[-1], stack[-preamble_length-1:-1]):
         stack.append(next(port_output))  # Should trap IterationError in case there is no invalid number...
     first_invalid_number = stack.pop()
