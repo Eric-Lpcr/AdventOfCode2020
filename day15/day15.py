298e98271000c1ff72e07a1b225691b260d908cf
Eric-Lpcr <lepicier.eric@gmail.com>
2020-12-15 19:33:51 +0100

    Day 14

---
+++ day15/day15.py
@@ -0,0 +1,51 @@
+
+def play_game1(starting_numbers, turns):
+    spoken = dict()
+    number = 0
+    for turn, number in enumerate(starting_numbers):
+        spoken[number] = [turn+1]
+    previous_number = number
+    for turn in range(len(starting_numbers)+1, turns+1):
+        if len(spoken[previous_number]) == 1:  # first seen
+            number = 0
+        else:
+            number = spoken[previous_number][0] - spoken[previous_number][1]
+        if number in spoken:
+            spoken[number] = [turn, spoken[number][0]]
+        else:
+            spoken[number] = [turn]
+        previous_number = number
+    return number
+
+
+def play_game(starting_numbers, turns):
+    spoken = dict()
+    number = 0
+    for turn, number in enumerate(starting_numbers):
+        spoken[number] = turn + 1
+    previous_number = number
+    for turn in range(len(starting_numbers)+1, turns+1):
+        if previous_number in spoken:
+            number = turn - 1 - spoken[previous_number]
+        else:
+            number = 0
+        spoken[previous_number] = turn - 1
+        previous_number = number
+    return number
+
+
+def main():
+    starting_numbers = [0, 13, 16, 17, 1, 10, 6]
+
+    end_number = play_game(starting_numbers, 2020)
+    print(f'Part 1 game gives {end_number}')
+
+    end_number = play_game(starting_numbers, 30000000)
+    print(f'Part 2 game gives {end_number}')
+
+
+if __name__ == '__main__':
+    main()
+
+# Test for part 1
+#     starting_numbers = [0, 3, 6]
