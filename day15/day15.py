52b41618576b9b743dd23a292d493d5d4b9c2161
Eric-Lpcr <lepicier.eric@gmail.com>
2020-12-15 23:52:06 +0100

    Day 15

--- day15/day15.py
+++ day15/day15.py
@@ -2,9 +2,9 @@ def play_game(starting_numbers, turns):
     spoken = dict()
     number = 0
     for turn, number in enumerate(starting_numbers):
-        spoken[number] = turn + 1
+        spoken[number] = turn
     previous_number = number
-    for turn in range(len(starting_numbers)+1, turns+1):
+    for turn in range(len(starting_numbers), turns):
         if previous_number in spoken:
             number = turn - 1 - spoken[previous_number]
         else:
@@ -12,6 +12,8 @@ def play_game(starting_numbers, turns):
         spoken[previous_number] = turn - 1
         previous_number = number
     return number
+
+# TODO Have a look at Van Eck sequence https://oeis.org/A181391
 
 
 def main():
