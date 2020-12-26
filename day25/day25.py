9d338af7581c78f85d24a70a2c4e76d2870fb135
Eric-Lpcr <lepicier.eric@gmail.com>
2020-12-26 18:12:19 +0100

    Day 25

---
+++ day25/day25.py
@@ -0,0 +1,58 @@
+from functools import lru_cache
+from itertools import count
+
+
+def loop(key, subject_number):
+    return key * subject_number % 20201227
+
+
+@lru_cache
+def compute_key_recurse(subject_number, loop_size):
+    if loop_size == 0:
+        return 1
+    else:
+        return loop(compute_key_recurse(subject_number, loop_size - 1), subject_number)
+
+
+def reverse_engineer(key, subject_number=7):
+    c = count(1)
+    try_key = 1
+    while loop_size := next(c):
+        if try_key := loop(try_key, subject_number) == key:
+            break
+    return loop_size
+
+
+def compute_key(subject_number, loop_size):
+    key = 1
+    for _ in range(loop_size):
+        key = loop(key, subject_number)
+    return key
+
+
+def main():
+    subject_number = 7
+
+    card_public_key = 17115212
+    door_public_key = 3667832
+
+    # example
+    # card_public_key = 5764801
+    # door_public_key = 17807724
+
+    card_loop_size = reverse_engineer(card_public_key, subject_number)
+    print(f'Card secret loop size is {card_loop_size}')
+
+    door_loop_size = reverse_engineer(door_public_key, subject_number)
+    print(f'Door secret loop size is {door_loop_size}')
+
+    card_encryption_key = compute_key(door_public_key, card_loop_size)
+    print(f'Card encryption key is {card_encryption_key}')
+
+    door_encryption_key = compute_key(card_public_key, door_loop_size)
+    print(f'Door encryption key is {door_encryption_key}')
+
+
+if __name__ == '__main__':
+    main()
+
